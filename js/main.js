c167821919043390ebd6bb084db24fb6e1723906
Nikhil yadav <yadavnikhil012@gmail.com>
2016-10-10 02:20:05 +0530

    pre-final website launch

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     //hamburger menu and overlay
     $('.trigger-menu').click(function(){
-            $("#cd-lateral-nav").css({"visibility":"visible","pointer-events":"initial"});
+            $("#cd-lateral-nav").css({"visibility":"visible"});
             $("#cd-lateral-nav").css("transform","translateX(0px)");
             $(".overlay").css({"display":"block","opacity":"0.6"});
     });
+    $(".links").click(function(){
+        $("#cd-lateral-nav").css({"visibility":"hidden"});
+        $("#cd-lateral-nav").css("transform","translateX(300px)");
+        $(".overlay").css({"display":"none","opacity":"0"});
+    });
     $(".exit").click(function(){
-        $("#cd-lateral-nav").css({"visibility":"hidden","pointer-events":"initial"});
+        $("#cd-lateral-nav").css({"visibility":"hidden"});
         $("#cd-lateral-nav").css("transform","translateX(300px)");
         $(".overlay").css({"display":"none","opacity":"0"});
     });
@@ -15,12 +20,20 @@ $(document).ready(function(){
             $('header').addClass("sticky");
             $(".scroll-btn").removeClass("hidden-md");
             $(".scroll-btn").removeClass("hidden-lg");
+            $(".brand").attr("src","images/logo.png");
+            $(".fa-bars").css("color","#333");
         }
     else{
         $('header').removeClass("sticky");
         $(".scroll-btn").addClass("hidden-md");
         $(".scroll-btn").addClass("hidden-lg");
+        $(".brand").attr("src","images/logo_white.png");
+        $(".fa-bars").css("color","#fff");
         }
+    });
+    $('.cover').particleground({
+        dotColor: '#5cbdaa',
+        lineColor: '#5cbdaa'
     });
 }
 );
