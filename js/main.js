ecb10ad0ebe765cdb39af31578082ab458b42469
Nikhil yadav <yadavnikhil012@gmail.com>
2016-10-11 03:48:26 +0530

    added hover effects in header and work and blog images; separated about me section; scroll smooth

--- js/main.js
+++ js/main.js
@@ -35,6 +35,18 @@ $(document).ready(function(){
         dotColor: '#5cbdaa',
         lineColor: '#5cbdaa'
     });
-}
-);
+    //smooth scrolling
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
